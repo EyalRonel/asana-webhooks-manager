64c2dcd6ca03391466804712b402f607d991d640
Eyal Ronel <roneleyal@gmail.com>
2017-02-20 09:03:53 +0200

    Base AWMController and an extending controller

---
+++ controllers/AWMController.js
@@ -0,0 +1,83 @@
+
+/**
+ * AWMController - A Base controller with utility methods
+ * Mainly used to validate auth, define a default JSON response structor and helper methods, etc
+ *
+ * @param {http.IncomingMessage}  req
+ * @param {http.ServerResponse}   res
+ *
+ * @returns {AWMController}
+ * */
+class AWMController {
+	constructor(req,res){
+
+		/**
+		 * request - {http.IncomingMessage}
+		 * */
+		this._request = req;
+
+		/**
+		 * response - Express response Object
+		 * */
+		this._response = res;
+
+
+		/**
+		 * protected {Boolean} - Determines if the controller requires authentication to access
+		 * */
+		this._protected = false;
+
+
+		/**
+		 * defaultMessages {Dictionary} - default textual messages for commonly used response codes
+		 * */
+		this._defaultMessages = {
+			100: "Continue",
+			101: "Switching Protocols",
+			200: "OK",
+			201: "Created",
+			202: "Accepted",
+			400: "Bed Request",
+			401: "Unauthorized",
+			403: "Forbidden",
+			404: "Not Found",
+			405: "Method Not Allowed"
+		}
+
+	}
+
+	/**
+	 * getDefaultMessageForCode
+	 *
+	 * @param {Integer} code
+	 * @return {String} Default HTTP Status code description
+	 * */
+	getDefaultMessageForCode(code) {
+		if (this._defaultMessages.hasOwnProperty(code)) { return this._defaultMessages[code]; }
+		else return "";
+	}
+
+	/**
+	 * response - a utility function to define a default JSON repsonse structure
+	 *
+	 * @param {Integer}       code - an HTTP response code
+	 * @param {<Any Object>}  data - A response payload
+	 * @param {String}        msg  - An optional message string
+	 * */
+	response(code,data,msg){
+
+		if (!code) throw new Error(this.constructor.name + " response must contain a status code");
+		if (!data) data = {};
+		if (!msg)  msg = this.getDefaultMessageForCode(code);
+
+		return this._response.status(code).json(
+			{
+				code: code,
+				data: data,
+				msg:  msg
+			}
+		);
+	}
+}
+
+module.exports = AWMController;
---
+++ controllers/root.js
@@ -0,0 +1,11 @@
+const AWMController = require('./AWMController');
+
+class RootController extends AWMController{
+	constructor(req,res){
+		super(req,res);
+	}
+
+
+}
+
+module.exports = RootController;
