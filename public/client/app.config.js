67dac67dae39bee7820df716c6f45fab6cd0db6b
Eyal Ronel <roneleyal@gmail.com>
2017-03-04 09:09:42 +0200

    Config file

---
+++ public/client/app.config.js
@@ -0,0 +1,12 @@
+(function(){
+
+	var config = {}
+
+	config.BASE_API_URL = "";
+	config.BASE_ASANA_API_URL = config.BASE_API_URL + "/asana";
+	config.ASANA_API_CURRENT_USER = config.BASE_ASANA_API_URL + "/me";
+	config.ASANA_API_WORKSPACES = config.BASE_ASANA_API_URL + "/workspaces";
+
+	awmApp.constant('config',config);
+
+})();
