cd0db987671704472cd2096c6f6dee09b384741c
Eyal Ronel <roneleyal@gmail.com>
2017-03-04 09:18:29 +0200

    Update oauth server endpoint

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
 const asanaClient = require('../helpers/asanaClient');
 
-// router.use(someMiddleware);
+/**
+ * /oauth/login
+ *
+ * Redirects the user to Asana's redirect uri
+ * */
+router.get('/asana/login',function(req,res){
+  var client = asanaClient();
+  res.redirect(client.app.asanaAuthorizeUrl())
+});
 
+
+/**
+ * /oauth/asana
+ *
+ * "Redirect URI endpoint" for Asana to redirect the user back to once authentication is completed
+ * */
 router.get('/asana',function(req,res){
 
   var code = req.query.code;
@@ -17,5 +31,7 @@ router.get('/asana',function(req,res){
   }
 
 });
+
+
 
 module.exports = router;
