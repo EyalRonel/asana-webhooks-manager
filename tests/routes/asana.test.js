5bccf6b513702979c9765d540aabda9b9acc94d4
Eyal Ronel <roneleyal@gmail.com>
2017-03-05 08:43:29 +0200

    Tests for routes/asana

--- tests/routes/asana.test.js
+++ tests/routes/asana.test.js
@@ -3,28 +3,38 @@ const expect = require('expect');
 var app = require('../../server.js').app;
 var nock = require('nock');
 
+
+/**
+ * mockResponse - A mock response object to test against
+ * */
+var mockResponse = 		{
+	"data": {
+		"workspaces": [
+			{
+				"id": 1337,
+				"name": "My Favorite Workspace"
+			}
+		],
+		"id": 5678,
+		"name": "Greg Sanchez",
+		"email": "gsanchez@example.com"
+	}
+};
+
+/**
+ * Define mock API response using Nock
+ * */
 var asanaApi = nock('https://app.asana.com/api/1.0/')
 	.get('/users/me')
-	.reply(200,
-		{
-			"data": {
-				"workspaces": [
-					{
-						"id": 1337,
-						"name": "My Favorite Workspace"
-					}
-				],
-				"id": 5678,
-				"name": "Greg Sanchez",
-				"email": "gsanchez@example.com"
-			}
-		}
-	);
+	.reply(200,mockResponse);
 
 
+/**
+ * Tests for routes/asana.js
+ * */
 describe('Asana Routes',function(){
 
-	it('Makre sure a token is present',(done) => {
+	it('Should prevent access for requests without a tokent',(done) => {
 		request(app)
 			.get("/asana/me")
 			.expect(400)
@@ -37,7 +47,10 @@ describe('Asana Routes',function(){
 			.get("/asana/me")
 			.set('Cookie',['token=123456'])
 			.expect(function(res){
-				if (res.body.data.name != "Greg Sanchez") throw new Error('Wrong user');
+				if (res.body.data.id != mockResponse.data.id) throw new Error('Wrong id');
+				if (res.body.data.name != mockResponse.data.name) throw new Error('Wrong user name');
+				if (res.body.data.email != mockResponse.data.email) throw new Error('Wrong email');
+				if (res.body.data.workspaces[0].id != mockResponse.data.workspaces[0].id) throw new Error('Invalid workspaces');
 			})
 			.expect(200,done);
 	});
