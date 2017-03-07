838e352254937a77ac4eb023b674236dad8e3b91
Eyal Ronel <roneleyal@gmail.com>
2017-03-07 07:24:24 +0200

    Added API endpoints for webhooks, projects, and workspace listing

--- routes/asana.js
+++ routes/asana.js
@@ -21,6 +21,50 @@ router.get('/me',function(req,res) {
 		.catch(function (err) { return response(res, 400, {}, err); });
 });
 
+/**
+ * GET /workspaces - returns a list of workspaces
+ * */
+router.get('/workspaces',function(req,res) {
+
+	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
+
+	var token = req.cookies.token;
+
+	var client = asanaClient(token);
+
+	client.workspaces.findAll()
+		.then(function (workspaces) {
+			return res.json(workspaces.data);
+			//return response(res, 200, workspaces.data,"");
+		})
+		.catch(function (err) {
+			//return response(res, 400, {}, err);
+			return res.json({"err":err});
+		});
+});
+
+/**
+ * GET /projects - returns a list of projects for a provided workspace
+ * */
+router.get('/projects/:workspaceId',function(req,res) {
+
+	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
+	if (!req.params.workspaceId) return response(res,400,{},"Missing workspaceId");
+
+	var token = req.cookies.token;
+
+	var client = asanaClient(token);
+
+	client.projects.findByWorkspace(req.params.workspaceId)
+		.then(function (workspaces) {
+			return response(res, 200, workspaces.data,"");
+		})
+		.catch(function (err) {
+			return response(res, 400, {}, err);
+
+		});
+});
+
 
 /**
  * GET /webhooks/<workspaceId> - Returns a list of all available webhooks for a workspace
@@ -36,7 +80,7 @@ router.get('/webhooks/:workspaceId',function(req,res) {
 	var client = asanaClient(token);
 
 	client.webhooks.getAll(workspaceId)
-		.then(function (response) { return response(res, 200, response,"!");})
+		.then(function (response) { return response(res, 200, response);})
 		.catch(function (err) { return response(res, 400, response, err); });
 });
 
