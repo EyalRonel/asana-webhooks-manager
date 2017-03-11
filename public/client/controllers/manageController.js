c186bd6f725d58a7c2dea20d5bfc7a5dd9a23817
Eyal Ronel <roneleyal@gmail.com>
2017-03-11 21:32:43 +0200

    List projects for each workspace

--- public/client/controllers/manageController.js
+++ public/client/controllers/manageController.js
@@ -1,14 +1,15 @@
 (function(){
 
-	var manageController = function($scope,userService, asanaService,navigationService, user){
+	var manageController = function($scope, $timeout, userService, asanaService,navigationService, user){
 
 		this.$scope = $scope;
+		this.$timeout = $timeout;
 		this.userService = userService;
 		this.asanaService = asanaService;
 		this.navigationService = navigationService;
 
 		this.workspaces = [];
-
+		
 		this.init();
 
 	};
@@ -22,18 +23,29 @@
 		this.workspaces = this.userService.getUser().getWorkspaces();
 	};
 
-	manageController.prototype.getProjects = function(workspaceId){
+	manageController.prototype.getProjects = function(workspaceIndex){
+
+		var workspaceId = this.workspaces[workspaceIndex].getId();
 		this.asanaService.getProjects(workspaceId).then(
 			function(projects){
 
-				//TODO: handle projects response
-				console.log(projects);
+				var projectsArray = [];
+				for (var i=0;i<projects.length;i++){
+					projectsArray.push(new AWM.Project(projects[i].id,projects[i].name));
+				}
+
+				this.userService.getUser().getWorkspaces()[workspaceIndex].setProjects(projectsArray);
+
+				this.$timeout(function(){
+					this.$scope.$apply();
+				}.bind(this),0);
+
 			}.bind(this),
 			function(error){}.bind(this)
 		);
 	};
 
 
-	awmApp.controller('manageController', ['$scope', 'userService', 'asanaService','navigationService','user',manageController]);
+	awmApp.controller('manageController', ['$scope', '$timeout','userService', 'asanaService','navigationService','user',manageController]);
 
 })();
