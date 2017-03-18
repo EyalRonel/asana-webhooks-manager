6359621d5b83f334698b19d6a8ba818e441f54d7
Eyal Ronel <roneleyal@gmail.com>
2017-03-18 19:08:41 +0200

    Moved token verification into route.all()

--- routes/asana.js
+++ routes/asana.js
@@ -3,19 +3,24 @@ const asanaClient = require('../helpers/asanaClient');
 const response    = require('../helpers/response');
 const restrictedAcess  = require('../middlewares/restrictedAccess');
 
+var client = null;
+
+router.all('/*',function(req,res,next){
+
+	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
+	client = asanaClient(req.cookies.token);
+
+	next();
+
+});
+
+
 router.use(restrictedAcess);
 
 /**
  * GET /me - returns the currently logged in user object
  * */
 router.get('/me',function(req,res) {
-
-	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
-
-	var token = req.cookies.token;
-
-	var client = asanaClient(token);
-
 	client.users.me()
 		.then(function (me) { return response(res, 200, me,"");})
 		.catch(function (err) { return response(res, 400, {}, err); });
@@ -25,20 +30,11 @@ router.get('/me',function(req,res) {
  * GET /workspaces - returns a list of workspaces
  * */
 router.get('/workspaces',function(req,res) {
-
-	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
-
-	var token = req.cookies.token;
-
-	var client = asanaClient(token);
-
 	client.workspaces.findAll()
 		.then(function (workspaces) {
 			return res.json(workspaces.data);
-			//return response(res, 200, workspaces.data,"");
 		})
 		.catch(function (err) {
-			//return response(res, 400, {}, err);
 			return res.json({"err":err});
 		});
 });
@@ -48,20 +44,12 @@ router.get('/workspaces',function(req,res) {
  * */
 router.get('/webhooks/:workspaceId',function(req,res) {
 
-	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
 	if (!req.params.workspaceId) return response(res,400,{},"Missing workspaceId");
-
-	var token = req.cookies.token;
 	var workspaceId = req.params.workspaceId;
 
-	var client = asanaClient(token);
-
 	client.webhooks.getAll(workspaceId)
-		.then(function (webhooks) {
-			return response(res, 200, webhooks.data);}
-		)
+		.then(function (webhooks) { return response(res, 200, webhooks.data); })
 		.catch(function (err) { return response(res, 400, {msg:err}, err); });
-
 
 });
 
@@ -70,13 +58,9 @@ router.get('/webhooks/:workspaceId',function(req,res) {
  * */
 router.post('/webhooks/:resourceId',function(req,res) {
 
-	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
 	if (!req.params.resourceId) return response(res, 400, {}, "Missing resourceId");
-
-	var token = req.cookies.token;
 	var resourceId = req.params.resourceId;
 
-	var client = asanaClient(token);
 	client.webhooks.create(resourceId, "https://" + req.get('host') + "/events/incoming/"+resourceId).then(
 		function (response) {
 			return res.status(200).json(response);
@@ -91,13 +75,10 @@ router.post('/webhooks/:resourceId',function(req,res) {
  * */
 router.delete('/webhooks/:webhookId',function(req,res){
 
-	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
 	if (!req.params.webhookId) return response(res, 400, {}, "Missing webhookId");
 
-	var token = req.cookies.token;
 	var webhookId = req.params.webhookId;
 
-	var client = asanaClient(token);
 	client.webhooks.deleteById(webhookId).then(
 		function (response) {
 			return res.status(200).json({code:200,data:{},msg:"Done!"});
@@ -113,12 +94,8 @@ router.delete('/webhooks/:webhookId',function(req,res){
  * */
 router.get('/projects/:workspaceId',function(req,res) {
 
-	if (!req.cookies.token) return response(res, 400, {}, "Missing token");
 	if (!req.params.workspaceId) return response(res,400,{},"Missing workspaceId");
-
-	var token = req.cookies.token;
 	var workspaceId = req.params.workspaceId;
-	var client = asanaClient(token);
 
 	var retval = [];
 
