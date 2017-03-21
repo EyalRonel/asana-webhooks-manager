ae63ae0174f5ce17bc4100b337f33482cc39177a
Eyal Ronel <eyal@priority.jobs>
2017-03-21 11:19:27 +0200

    removed unused code

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path    = require("path");
-const asanaClient = require('../helpers/asanaClient');
 
 var registerRoutes = function(app){
 
@@ -10,14 +9,6 @@ var registerRoutes = function(app){
 
 	router.get('/',function(req,res){
 		res.sendFile(path.join(__dirname,'../public/client/views','index.html'));
-	});
-
-
-	router.get('/test',function(req,res,next){
-
-		const rootController = require('../controllers/root');
-		var rootCtrl =  new rootController(req,res).test();
-
 	});
 
 };
