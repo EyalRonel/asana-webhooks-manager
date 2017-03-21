0ac84741c436bc37bce2713b8ef2937745866019
Eyal Ronel <eyal@priority.jobs>
2017-03-21 18:27:09 +0200

    Event routes tests

---
+++ tests/routes/events.test.js
@@ -0,0 +1,61 @@
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+var supertest = require('supertest');
+var expect = require('expect');
+var express = require('express');
+const bodyParser = require('body-parser');
+
+const eventsController = require('../../controllers/EventsController');
+
+describe('Events route', function () {
+
+	var app,
+		request,
+		route;
+
+	beforeEach(function () {
+
+		app = express();
+		app.use( bodyParser.json());
+		app.use(bodyParser.urlencoded({extended: true}));
+		route = proxyquire('../../routes/events', {})(app);
+		request = supertest(app);
+
+	});
+
+	afterEach(function(){
+	});
+
+
+	it('Should handshake in-flight webhook creation requests', function (done) {
+
+		var xHookSecretValue = "ABCDE";
+
+		request
+			.post('/events/incoming/123')
+			.set('X-Hook-Secret', xHookSecretValue)
+			.field('events', [])
+			.send({events:[]})
+			.end(function(err,res){
+				expect(res.statusCode).toBe(200);
+				expect(res.header['x-hook-secret']).toBe(xHookSecretValue);
+				done();
+			});
+
+	});
+
+
+	it('Should pass event handling to eventController when X-Hook-Secret header is not present', function (done) {
+
+		request
+			.post('/events/incoming/123')
+			.field('events', [])
+			.send({events:[]})
+			.end(function(err,res){
+				expect(res.statusCode).toBe(200);
+				done();}
+			);
+
+	});
+
+});
