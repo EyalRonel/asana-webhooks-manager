baab689e06ea9f61d1f715bb56d1ba207640c26e
Eyal Ronel <eyal@priority.jobs>
2017-03-25 19:53:32 +0300

    Configuration tests

---
+++ tests/helpers/config.test.js
@@ -0,0 +1,46 @@
+/**
+ * Libraries
+ * */
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const expect = require('expect');
+
+var fakeConfig={
+	clientId: "ABCDEFG",
+	clientSecret: "1234567",
+	redirectUri: "http://www.fakeurl.com"
+};
+
+/**
+ * Module under test
+ * */
+const configHelper = proxyquire('../../helpers/configHelper', {'../config/asana':fakeConfig}
+);
+
+
+/**
+ * Tests
+ * */
+describe('Config helper',function(){
+
+	beforeEach(function () {
+
+	});
+
+	afterEach(function(){
+
+	});
+
+	it('Return a client id',function(){
+		expect(configHelper.getClientId()).toBe(fakeConfig.clientId);
+	});
+
+	it('Return a client secret',function(){
+		expect(configHelper.getClientSecret()).toBe(fakeConfig.clientSecret);
+	});
+
+	it('Return a re',function(){
+		expect(configHelper.getRediectUri()).toBe(fakeConfig.redirectUri);
+	});
+
+});
