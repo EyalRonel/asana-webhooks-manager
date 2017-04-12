50595985a6e9078e53f811adb95f997a07ad1ea1
Eyal Ronel <roneleyal@gmail.com>
2017-04-12 18:59:41 +0300

    Verify events payload with HMAC256

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -16,7 +16,11 @@ class EventsController extends AWMController {
 	}
 
 	onIncomingEvents(){
+
 		if (this.request().get('X-Hook-Secret') != null) return this.handshake();
+		else if (this.request().get('X-Hook-Signature') != null) return this.handle();
+		else return this.reply(403,{});
+
 	}
 
 	handshake(){
@@ -24,11 +28,19 @@ class EventsController extends AWMController {
 		//Get X-Hook-Secret form the request object
 		var xHookSecret = this.request().get('X-Hook-Secret');
 
-		//Store webhook secret for validatin incoming event request
+		//Store webhook secret for validating incoming event request
 		mongodb.getConnection();
-		new AWMWebhook({
+		return new AWMWebhook({
 			resource_id: this.request().params.resourceId,
 			secret: xHookSecret
+		}).save()
+			.then(function(){
+				//Response to in-flight webhook creation request
+				this.response().set('X-Hook-Secret',xHookSecret);
+				return this.reply(200,{});
+			}.bind(this))
+			.catch();
+
 
 
 	}
@@ -36,15 +48,53 @@ class EventsController extends AWMController {
 	handle(){
 
 
+		//Verify signature header exists
+		var xHookSignatureHeader = this.request().get('X-Hook-Signature');
+		if (xHookSignatureHeader == null) return this.reply(403,{},"Unauthorized request");
+
+		//Verify webhook is listed internally
 		mongodb.getConnection();
+		return AWMWebhook.findOne({resource_id: this.request().params.resourceId.toString()}).exec().then(function(webhook){
+
+			//Webhook was not found, deny request
+			if (typeof webhook == "undefined" || webhook == null || webhook.length == 0) return this.reply(400, {},"Unknown webhook");
+
+			//Match encrypted request payload against header header, using secret from original webhook handshake
+			var encryptedRequestBody = CryptoJS.HmacSHA256(JSON.stringify(this.request().body),webhook.secret).toString();
+			if (xHookSignatureHeader !== encryptedRequestBody) return this.reply(403,{},"Unauthorized request");
+      else {
+
+				//At this point the request is fully validated and can be processed
+				var eventsArray = this.request().body.events;
+				AWMEvent.insertMany(eventsArray, function(error, docs) {});
+
+				this.socket.emit('events', this.request().body.events);
+
+				return this.reply(200,{});
+			}
+		}.bind(this));
 
 
+		//query.exec(function (err, docs) {
 
+			////Webhook was not found, deny request
+			//if (docs.length == 0) return this.reply(400, {},"Unknown webhook");
+			//
+			////Match encrypted request payload against header header, using secret from original webhook handshake
+			//var encryptedRequestBody = CryptoJS.HmacSHA256(JSON.stringify(this.request().body),docs[0].secret).toString();
+			//if (xHookSignatureHeader !== encryptedRequestBody) return this.reply(403,{},"Unauthorized request");
+			//
+			////At this point the request is fully validated and can be processed
+			//var eventsArray = this.request().body.events;
+			//AWMEvent.insertMany(eventsArray, function(error, docs) {});
+			//
+			//this.socket.emit('events', this.request().body.events);
+			//
+			//return this.reply(200,{});
 
+		//}.bind(this));
 
-
-
-
+	}
 
 }
 
